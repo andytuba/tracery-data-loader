a1b4449929e98e345ad0446ca4d70a9ea7769172
Andy Tuba <andytuba@andytuba.com>
2020-12-27 23:25:37 -0800

    flesh out compare
    lol

--- __tests__/compare.test.js
+++ __tests__/compare.test.js
@@ -11,14 +11,82 @@ const compare = _compare({
     someString: "is a string",
 });
 
-test("simple string equality", () =>
+test("nonexistent variable name", () => {
+    expect(compare("doesNotExist", "=", "")).toEqual(false);
+});
+
+test("simple string equality", () => {
     expect(compare("someString", "=", "is a string")).toEqual(true);
+    expect(compare("someString", "==", "is a string")).toEqual(true);
 });
 
-test("simple number equality", () =>
+test("simple number equality", () => {
+    expect(compare("day", "=", "4")).toEqual(false);
     expect(compare("day", "=", "5")).toEqual(true);
+    expect(compare("day", "=", "6")).toEqual(false);
+
+    expect(compare("day", "==", "4")).toEqual(false);
+    expect(compare("day", "==", "5")).toEqual(true);
+    expect(compare("day", "==", "6")).toEqual(false);
 });
 
-test("simple number less-than", () =>
+test("simple number greater-than", () => {
     expect(compare("day", ">", "4")).toEqual(true);
+    expect(compare("day", ">", "5")).toEqual(false);
+    expect(compare("day", ">", "6")).toEqual(false);
+});
+
+test("simple number greater-than-equals", () => {
+    expect(compare("day", ">=", "4")).toEqual(true);
+    expect(compare("day", ">=", "5")).toEqual(true);
+    expect(compare("day", ">=", "6")).toEqual(false);
+});
+
+test("simple number less-than", () => {
+    expect(compare("day", "<", "4")).toEqual(false);
+    expect(compare("day", "<", "5")).toEqual(false);
+    expect(compare("day", "<", "6")).toEqual(true);
+});
+
+test("simple number less-than-equals", () => {
+    expect(compare("day", "<=", "4")).toEqual(false);
+    expect(compare("day", "<=", "5")).toEqual(true);
+    expect(compare("day", "<=", "6")).toEqual(true);
+});
+
+test("simple string begins-with", () => {
+    expect(compare("someString", "^=", "i")).toEqual(true);
+    expect(compare("someString", "^=", "is")).toEqual(true);
+    expect(compare("someString", "^=", "is a ")).toEqual(true);
+    expect(compare("someString", "^=", "is a string")).toEqual(true);
+
+    expect(compare("someString", "^=", "n")).toEqual(false);
+    expect(compare("someString", "^=", "nest c'est pas un string")).toEqual(false);
+});
+
+test("simple number begins-with", () => {
+    expect(compare("fullYear", "^=", "1")).toEqual(true);
+    expect(compare("fullYear", "^=", "19")).toEqual(true);
+
+    expect(compare("fullYear", "^=", "96")).toEqual(false);
+    expect(compare("fullYear", "^=", "6")).toEqual(false);
+});
+
+test("simple number ends-with", () => {
+    expect(compare("fullYear", "$=", "96")).toEqual(true);
+    expect(compare("fullYear", "$=", "6")).toEqual(true);
+
+    expect(compare("fullYear", "$=", "1")).toEqual(false);
+    expect(compare("fullYear", "$=", "19")).toEqual(false);
+});
+
+test("regex string", () => {
+    expect(compare("someString", "~", "[\w\s]+")).toEqual(true);
+    expect(compare("someString", "~", "\d+")).toEqual(false);
+});
+
+test("regex number", () => {
+    expect(compare("fullYear", "~", "^19")).toEqual(true);
+    expect(compare("fullYear", "~", "\\d{4}")).toEqual(true);
+    expect(compare("fullYear", "~", "^\\d{1,2}$")).toEqual(false);
 });
--- src/compare.js
+++ src/compare.js
@@ -1,6 +1,40 @@
 const compare = environment => (attribute, operator, value) => {
+    const expected = environment[attribute];
+    if (expected === undefined) {
+        return false;
+    }
+    if (Array.isArray(operator[0]) && typeof operator[1] === "string") {
+        // why does the parser parse this way ...
+        operator = operator[1];
+    }
 
+    // leaning heavily on JavaScript typecasting here
+    // deliberately using == and !=
 
+    if (operator === "=" || operator === "==") {
+        return expected == value;
+    }  else if (operator === "!=" || operator === "<>") {
+        return expected != value;
+    } else if (operator === ">") {
+        return expected > value;
+    } else if (operator === ">=") {
+        return expected >= value;
+    } else if (operator === "<") {
+        return expected < value;
+    } else if (operator === "<=") {
+        return expected <= value;
+    } else if (operator === "^=") {
+        return String(expected).startsWith(value);
+    } else if (operator === "$=") {
+        return String(expected).endsWith(value);
+    } else if (operator === "~=") {
+        return (Array.isArray(expected) ? expected : String(expected))
+            .includes(value);
+    } else if (operator === "~") {
+        return RegExp(value).test(expected);
+    }
+
+    console.error(`compare could not handle the operator ${operator} for compare("${attribute}", "${operator}", "${value}")`);
 }
 
 module.exports = compare;
